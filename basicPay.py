c4422698a4a865b454d5334e088eb776394c9ab4
dennis289 <113549311+dennis289@users.noreply.github.com>
2024-02-15 04:23:04 +0000

    after

--- basicPay.py
+++ basicPay.py
@@ -1,5 +1,8 @@
+# a prompt to the user to input credentials
 RatePerHour=float(input("Enter hourly rate"))
 HoursWorked=int(input("Enter the hours worked"))
+# The workoutdone using the inputed credentials
 GSalary= (RatePerHour*HoursWorked)
+# printing the gross pay
 print("The gross salary is: ")
 print(GSalary)
--- sphereArea.py
+++ sphereArea.py
@@ -1,4 +1,7 @@
+# prompting the user to enter the radius
 radius= float(input("Enter tha radius of a sphere"))
+# Calculating the sphere's area 
 area= (3*3.142*radius/4)
+# Printing the area
 print("The area of the sphere is equal to: ")
 print(area)
